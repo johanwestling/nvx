93b9ed91c927b9dcf5a8981e2932bfde2a6528d2
Johan Westling <johan@westling.ax>
2018-10-25 12:18:39 +0300

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -2,9 +2,13 @@
 
 source "${PWD}/nvx/helpers/functions.sh"
 
+nvx_reload_session="${1}"
+
 nvx_update_bashrc
 nvx_install_bins
 
 bash ${PWD}/nvx/interface/interface.sh
 
-exec bash
+if [ -z "${nvx_reload_session}" ]; then
+  exec bash
+fi
